245a5f28a1095e62e3cef1427eaba094bdec14ba
spreadpando <aphyd@aphyds-MBP.fios-router.home>
2022-01-22 17:29:05 -0500

    update setupTypeScript.js

--- client/scripts/setupTypeScript.js
+++ client/scripts/setupTypeScript.js
@@ -20,7 +20,7 @@ const { argv } = require("process")
 const projectRoot = argv[2] || path.join(__dirname, "..")
 
 // Add deps to pkg.json
-const packageJSON = JSON.parse(fs.readFileSync(path.join(projectRoot, "package.json"), "utf8"))
+const packageJSON = JSON.parse(fs.readFileSync(path.join(projectRoot, "../package.json"), "utf8"))
 packageJSON.devDependencies = Object.assign(packageJSON.devDependencies, {
   "svelte-check": "^2.0.0",
   "svelte-preprocess": "^4.0.0",
@@ -36,7 +36,7 @@ packageJSON.scripts = Object.assign(packageJSON.scripts, {
 })
 
 // Write the package JSON
-fs.writeFileSync(path.join(projectRoot, "package.json"), JSON.stringify(packageJSON, null, "  "))
+fs.writeFileSync(path.join(projectRoot, "../package.json"), JSON.stringify(packageJSON, null, "  "))
 
 // mv src/main.js to main.ts - note, we need to edit rollup.config.js for this too
 const beforeMainJSPath = path.join(projectRoot, "src", "main.js")
@@ -51,7 +51,7 @@ appFile = appFile.replace("export let name;", 'export let name: string;')
 fs.writeFileSync(appSveltePath, appFile)
 
 // Edit rollup config
-const rollupConfigPath = path.join(projectRoot, "rollup.config.js")
+const rollupConfigPath = path.join(projectRoot, "../rollup.config.js")
 let rollupConfig = fs.readFileSync(rollupConfigPath, "utf8")
 
 // Edit imports
@@ -82,7 +82,7 @@ const tsconfig = `{
   "include": ["src/**/*"],
   "exclude": ["node_modules/*", "__sapper__/*", "public/*"]
 }`
-const tsconfigPath =  path.join(projectRoot, "tsconfig.json")
+const tsconfigPath =  path.join(projectRoot, "../tsconfig.json")
 fs.writeFileSync(tsconfigPath, tsconfig)
 
 // Add global.d.ts
@@ -108,14 +108,14 @@ if (!argv[2]) {
 }
 
 // Adds the extension recommendation
-fs.mkdirSync(path.join(projectRoot, ".vscode"), { recursive: true })
-fs.writeFileSync(path.join(projectRoot, ".vscode", "extensions.json"), `{
+fs.mkdirSync(path.join(projectRoot, "../.vscode"), { recursive: true })
+fs.writeFileSync(path.join(projectRoot, "../.vscode", "extensions.json"), `{
   "recommendations": ["svelte.svelte-vscode"]
 }
 `)
 
 console.log("Converted to TypeScript.")
 
-if (fs.existsSync(path.join(projectRoot, "node_modules"))) {
+if (fs.existsSync(path.join(projectRoot, "../node_modules"))) {
   console.log("\nYou will need to re-run your dependency manager to get started.")
 }
